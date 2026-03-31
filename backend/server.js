f34b7bf8409252637f8f4ea101eeb10604a69c25
Wasif Raza <wasif.raazaa@gmail.com>
2026-03-31 21:27:16 +0500

    final update of CORS

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:4000",
-  "https://expense-tracker-frontend-mauve-five.vercel.app"
+  "https://expensetrackerfrontend-liard.vercel.app"
 ];
 
 app.use(cors({
